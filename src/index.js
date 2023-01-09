b05264c0fbc58c7955f8315fa36603d271d086ad
Anton Shabalin <wolferus.one@gmail.com>
2023-01-09 17:28:36 +0300

    Завершил header и nav ; начал модалку с обратной связью

--- src/index.js
+++ src/index.js
@@ -3,3 +3,12 @@ import './stylus/main.scss';
 // console.log('show json file', json);
 
 // alert('hello');
+
+document.getElementById('navOpen').addEventListener('click', function () {
+   document.getElementById('nav').style.left = 0;
+   document.getElementById('nav').style.boxShadow = '16px 0px 52px rgba(14, 24, 80, 0.2)';
+});
+document.getElementById('navClose').addEventListener('click', function () {
+   document.getElementById('nav').style.left = '-320px';
+   document.getElementById('nav').style.boxShadow = 'none';
+});
