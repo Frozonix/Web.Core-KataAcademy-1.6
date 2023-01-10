b1cf9ca9a338acfbd8fe3ec3def97498e005ba7f
Anton Shabalin <wolferus.one@gmail.com>
2023-01-10 15:20:40 +0300

    Попробую объединить стили модальных окон

--- src/index.js
+++ src/index.js
@@ -12,3 +12,21 @@ document.getElementById('navClose').addEventListener('click', function () {
    document.getElementById('nav').style.left = '-320px';
    document.getElementById('nav').style.boxShadow = 'none';
 });
+// ==================================================================
+document.getElementById('feedbackOpen').addEventListener('click', function () {
+   document.getElementById('modalFeedback').style.right = 0;
+   document.getElementById('modalFeedback').style.boxShadow = '-2px 0px 4px rgba(69, 79, 126, 0.02), 16px 0px 52px rgba(14, 24, 80, 0.2)';
+});
+document.getElementById('feedbackClose').addEventListener('click', function () {
+   document.getElementById('modalFeedback').style.right = '-520px';
+   document.getElementById('modalFeedback').style.boxShadow = 'none';
+});
+
+document.getElementById('feedbackOpen-header').addEventListener('click', function () {
+   document.getElementById('modalFeedback').style.right = 0;
+   document.getElementById('modalFeedback').style.boxShadow = '-2px 0px 4px rgba(69, 79, 126, 0.02), 16px 0px 52px rgba(14, 24, 80, 0.2)';
+});
+document.getElementById('feedbackClose').addEventListener('click', function () {
+   document.getElementById('modalFeedback').style.right = '-520px';
+   document.getElementById('modalFeedback').style.boxShadow = 'none';
+});
