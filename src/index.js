66e6ca35bcc92997824c74ef29e62b553bb7fe15
Anton Shabalin <wolferus.one@gmail.com>
2023-01-16 04:35:21 +0300

    Закончил Read More, починил Swiper, осталось обернуть блоки в Swiper

--- src/index.js
+++ src/index.js
@@ -1,8 +1,91 @@
 import './stylus/main.scss';
-// import json from './assets/json';
-// console.log('show json file', json);
 
-// alert('hello');
+// core version + navigation, pagination modules:
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+// configure Swiper to use modules
+Swiper.use([Navigation, Pagination]);
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+var swiper = new Swiper('.mySwiper', {
+   pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+   },
+});
+//==================================================================
+//Read more
+
+function readMore() {
+   let more1 = document.getElementById('more-1');
+   if (more1.style.display == 'unset') {
+      more1.style.display = 'none';
+      document.getElementById('readMore-1').querySelector('p').innerHTML = 'Читать далее';
+      document.getElementById('readMore-1').querySelector('img').style.transform = 'rotate(360deg)';
+   } else {
+      more1.style.display = 'unset';
+      document.getElementById('readMore-1').querySelector('p').innerHTML = 'Скрыть';
+      document.getElementById('readMore-1').querySelector('img').style.transform = 'rotate(180deg)';
+   }
+}
+
+function readMore2() {
+   let startHeight = 164;
+   let openHeight = document.querySelector('.content__brands-wrapper').offsetHeight;
+   console.log(openHeight);
+   if (readMore_2_isClose) {
+      document.querySelector('.content__brand-hide-wrapper').style.height = openHeight + 'px';
+      document.getElementById('readMore-2').querySelector('p').innerHTML = 'Скрыть';
+      document.getElementById('readMore-2').querySelector('img').style.transform = 'rotate(180deg)';
+
+      readMore_2_isClose = false;
+   } else {
+      document.querySelector('.content__brand-hide-wrapper').style.height = startHeight + 'px';
+      document.getElementById('readMore-2').querySelector('p').innerHTML = 'Показать все';
+      document.getElementById('readMore-2').querySelector('img').style.transform = 'rotate(360deg)';
+      readMore_2_isClose = true;
+   }
+}
+
+function readMore3() {
+   let startHeight = 162;
+   let openHeight = document.querySelector('.content__types-wrapper').offsetHeight;
+   console.log(openHeight);
+   if (readMore_3_isClose) {
+      document.querySelector('.content__type-hide-wrapper').style.height = openHeight + 'px';
+      document.getElementById('readMore-3').querySelector('p').innerHTML = 'Скрыть';
+      document.getElementById('readMore-3').querySelector('img').style.transform = 'rotate(180deg)';
+
+      readMore_3_isClose = false;
+   } else {
+      document.querySelector('.content__type-hide-wrapper').style.height = startHeight + 'px';
+      document.getElementById('readMore-3').querySelector('p').innerHTML = 'Показать все';
+      document.getElementById('readMore-3').querySelector('img').style.transform = 'rotate(360deg)';
+      readMore_3_isClose = true;
+   }
+   // if (screen_width >= 1100) {
+   //    openHeight = document.querySelectorAll('.brand-items--1100');
+   // } else {
+   //    openHeight = document.querySelectorAll('.brand-items--768');
+   // }
+}
+document.getElementById('readMore-1').addEventListener('click', function (e) {
+   console.log(e.target.id);
+   readMore();
+});
+let readMore_2_isClose = true;
+document.getElementById('readMore-2').addEventListener('click', function () {
+   readMore2();
+});
+let readMore_3_isClose = true;
+document.getElementById('readMore-3').addEventListener('click', function () {
+   readMore3();
+});
+//==================================================================
+//Открытие и закрытие navbar
 
 document.getElementById('navOpen').addEventListener('click', function () {
    document.getElementById('nav').style.left = 0;
@@ -14,7 +97,7 @@ document.getElementById('navClose').addEventListener('click', function () {
 });
 //==================================================================
 //Вспомогательные функции для модальных окон
-// let modalIsOpen = false;
+
 function setModalId(target) {
    let id = target.substr(0, 4);
    let result = 'modal' + id.charAt(0).toUpperCase() + id.slice(1);
